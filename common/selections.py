a6b9d87297d10a417b2f3965a2316dcff382f43f
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2024-12-08 09:29:25 +0100

    some missing selectionsw

--- common/selections.py
+++ common/selections.py
@@ -14,15 +14,15 @@ ptH_ind = data_structure.feature_names.index("DER_pt_h")
 # Put the elements of your selections here, i.e., cuts you want to apply
 selections = {
         "inclusive"   : lambda data: data,
-        "lowMT"       : lambda data: data[data[:,MT_ind]<70],
-        "highMT"      : lambda data: data[data[:,MT_ind]>=70],
-        "VBFJet"      : lambda data: data[(data[:,LJ_ind]>50) & (data[:,SLJ_ind]>30)],
-        "noVBFJet"    : lambda data: data[~( (data[:,LJ_ind]>50) & (data[:,SLJ_ind]>30))],
-        "ptH0to50"    : lambda data: data[ data[:,ptH_ind]<50],
-        "ptH0to100"   : lambda data: data[ data[:,ptH_ind]<100],
-        "ptH50to100"  : lambda data: data[ (50<=data[:,ptH_ind]) & (data[:,ptH_ind]<100)],
-        "ptH100to200" : lambda data: data[ (100<=data[:,ptH_ind])&(data[:,ptH_ind]<200)],
-        "ptH100"      : lambda data: data[ (100<=data[:,ptH_ind])],
+        "lowMT"       : lambda data: data[      data[:,MT_ind]<70  ],
+        "highMT"      : lambda data: data[      data[:,MT_ind]>=70 ],
+        "VBFJet"      : lambda data: data[     (data[:,LJ_ind]>50) & (data[:,SLJ_ind]>30)],
+        "noVBFJet"    : lambda data: data[   ~((data[:,LJ_ind]>50) & (data[:,SLJ_ind]>30))],
+        "ptH0to50"    : lambda data: data[      data[:,ptH_ind]<50],
+        "ptH0to100"   : lambda data: data[      data[:,ptH_ind]<100],
+        "ptH50to100"  : lambda data: data[(50 <=data[:,ptH_ind]) & (data[:,ptH_ind]<100)],
+        "ptH100to200" : lambda data: data[(100<=data[:,ptH_ind]) & (data[:,ptH_ind]<200)],
+        "ptH100"      : lambda data: data[ 100<=data[:,ptH_ind]],
         "ptH200"      : lambda data: data[ 200<=data[:,ptH_ind]],
     }
 
@@ -38,6 +38,7 @@ for s in [
     "lowMT_noVBFJet_ptH100", 
     "lowMT_noVBFJet_ptH200",
     "highMT_noVBFJet_ptH0to50",
+    "highMT_noVBFJet_ptH0to100",
     "highMT_noVBFJet_ptH50to100",
     "highMT_noVBFJet_ptH100to200",
     "highMT_noVBFJet_ptH100",
@@ -46,6 +47,7 @@ for s in [
     "noVBFJet_ptH50to100",
     "noVBFJet_ptH100to200",
     "noVBFJet_ptH100to200"
+    "noVBFJet_ptH200"
     ]:
     c_fs = [selections[k] for k in s.split('_')]
     def selector(data, c_fs=c_fs):
@@ -63,4 +65,3 @@ def print_all():
 
 # from selections import lowMT_VBFJet
 globals().update(selections)
-
