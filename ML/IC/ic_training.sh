6b738e0035b2e5b779909eed122ec083053d39c4
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2024-12-09 21:08:58 +0100

    counts in IC

--- ML/IC/ic_training.sh
+++ ML/IC/ic_training.sh
@@ -1,8 +1,9 @@
-python ic_training.py --selection lowMT_VBFJet
-python ic_training.py --selection highMT_VBFJet
-python ic_training.py --selection lowMT_noVBFJet_ptH0to100
-python ic_training.py --selection lowMT_noVBFJet_ptH100to200
-python ic_training.py --selection lowMT_noVBFJet_ptH200
-python ic_training.py --selection highMT_noVBFJet_ptH0to100
-python ic_training.py --selection highMT_noVBFJet_ptH100to200
-python ic_training.py --selection highMT_noVBFJet_ptH200
+python ic_training.py --overwrite --selection inclusive
+python ic_training.py --overwrite --selection lowMT_VBFJet
+python ic_training.py --overwrite --selection highMT_VBFJet
+python ic_training.py --overwrite --selection lowMT_noVBFJet_ptH0to100
+python ic_training.py --overwrite --selection lowMT_noVBFJet_ptH100to200
+python ic_training.py --overwrite --selection lowMT_noVBFJet_ptH200
+python ic_training.py --overwrite --selection highMT_noVBFJet_ptH0to100
+python ic_training.py --overwrite --selection highMT_noVBFJet_ptH100to200
+python ic_training.py --overwrite --selection highMT_noVBFJet_ptH200
