716b7a41068861a51e95a7e16fb5e61386f33c88
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2024-12-11 11:13:05 +0100

    demos etc.

--- ML/ICP/icp_training.sh
+++ ML/ICP/icp_training.sh
@@ -19,14 +19,10 @@
 
 python icp_training.py --config icp_quad_tes_jes_met --selection lowMT_VBFJet
 python icp_training.py --config icp_quad_tes_jes_met --selection highMT_VBFJet
-python icp_training.py --config icp_quad_tes_jes_met --selection lowMT_noVBFJet_ptH0to100
+#python icp_training.py --config icp_quad_tes_jes_met --selection lowMT_noVBFJet_ptH0to100
 python icp_training.py --config icp_quad_tes_jes_met --selection lowMT_noVBFJet_ptH100
-#python icp_training.py --config icp_quad_tes_jes_met --selection lowMT_noVBFJet_ptH100to200
-#python icp_training.py --config icp_quad_tes_jes_met --selection lowMT_noVBFJet_ptH200
 python icp_training.py --config icp_quad_tes_jes_met --selection highMT_noVBFJet_ptH0to100
 python icp_training.py --config icp_quad_tes_jes_met --selection highMT_noVBFJet_ptH100
-#python icp_training.py --config icp_quad_tes_jes_met --selection highMT_noVBFJet_ptH100to200
-#python icp_training.py --config icp_quad_tes_jes_met --selection highMT_noVBFJet_ptH200
 
 #python icp_training.py --config icp_quad_jes --selection lowMT_VBFJet
 #python icp_training.py --config icp_quad_jes --selection highMT_VBFJet
--- ML/TFMC/tfmc_training.sh
+++ ML/TFMC/tfmc_training.sh
@@ -3,7 +3,7 @@
 
 python tfmc_training.py --config tfmc_2_reg --selection  lowMT_VBFJet
 python tfmc_training.py --config tfmc_2_reg --selection  highMT_VBFJet
-python tfmc_training.py --config tfmc_2_reg --selection  lowMT_noVBFJet_ptH0to100 --n_split 50
+#python tfmc_training.py --config tfmc_2_reg --selection  lowMT_noVBFJet_ptH0to100 --n_split 50
 python tfmc_training.py --config tfmc_2_reg --selection  lowMT_noVBFJet_ptH100
 python tfmc_training.py --config tfmc_2_reg --selection  highMT_noVBFJet_ptH0to100
 python tfmc_training.py --config tfmc_2_reg --selection  highMT_noVBFJet_ptH100
