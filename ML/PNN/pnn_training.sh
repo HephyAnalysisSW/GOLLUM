32ab84615969ec630c66d615f204cf953908dc07
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2024-12-17 21:07:12 +0100

    new trainings

--- ML/PNN/pnn_training.sh
+++ ML/PNN/pnn_training.sh
@@ -4,8 +4,8 @@ python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_quad_jes_v
 python pnn_training.py --selection highMT_VBFJet --config pnn_quad_jes_v2 --training v4 --every 1
 python pnn_training.py --selection highMT_noVBFJet --config pnn_quad_jes_v2 --training v4 --every 1
 
-python pnn_training.py --selection lowMT_VBFJet --config pnn_quad_tes_v2 --training v4 --every 1
-python pnn_training.py --selection lowMT_noVBFJet_ptH0to100 --config pnn_quad_tes_v2 --n_split 50 --training v4 --every 1
-python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_quad_tes_v2 --training v4 --every 1
-python pnn_training.py --selection highMT_VBFJet --config pnn_quad_tes_v2 --training v4 --every 1
-python pnn_training.py --selection highMT_noVBFJet --config pnn_quad_tes_v2 --training v4 --every 1
+python pnn_training.py --selection lowMT_VBFJet --config pnn_quad_tes_v2 --training v5 --every 1
+python pnn_training.py --selection lowMT_noVBFJet_ptH0to100 --config pnn_quad_tes_v2 --n_split 50 --training v5 --every 1
+python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_quad_tes_v2 --training v5 --every 1
+python pnn_training.py --selection highMT_VBFJet --config pnn_quad_tes_v2 --training v5 --every 1
+python pnn_training.py --selection highMT_noVBFJet --config pnn_quad_tes_v2 --training v5 --every 1
