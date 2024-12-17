4bd87ec7e5ad357a0d2a8f7bee93121d95c92544
Dennis Schwarz <dennis.schwarz@cern.ch>
2024-12-17 11:43:29 +0100

    clean up dennis

--- user/dennis/minuitTest.py
+++ user/dennis/minuitTest.py
@@ -7,87 +7,87 @@ class likelihoodFit:
         self.features = features
 
     def likelihood_function(self, mu, nu1, nu2):
+        # this defines the function that is minimized
         penalty = 0.5*(nu1**2+nu2**2)
         return (mu - 1.1**nu1)**2 + (mu + 2.1**nu2)**2 + mu * mu + penalty
 
     def fit(self):
+        # function to find the global minimum, minimizing mu and nus
         print("Fit global minimum")
         errordef = Minuit.LEAST_SQUARES
         m = Minuit(self.likelihood_function, mu=0.0, nu1=0.0, nu2=0.0)
         m.migrad()
-        # print("Minimum value:", m.fval)
-        # print("Best parameters:", m.values)
         return m.fval, m.values
 
-    def scan(self):
+    def scan(self, Npoints=100, mumin=-5, mumax=5):
+        # Scan over points of mu
         print("Scan signal strength")
-        qPoints = []
-        Nsteps = 100
-        mumin = -5
-        mumax = 5
-        muList = [mumin+i*(mumax-mumin)/Nsteps for i in range(Nsteps)]
+        # First find global Min and store MLE values
+        q_mle, parameters_mle = self.fit()
+        # Now make scan over nu
+        qDeltas = []
+        muList = [mumin+i*(mumax-mumin)/Npoints for i in range(Npoints)]
         errordef = Minuit.LEAST_SQUARES
         for mu in muList:
+            # Create a function that fixes mu and only uses the nu as arguments
             fixed_mu = mu
             likelihood_fixedMu = lambda nu1, nu2: self.likelihood_function(fixed_mu, nu1, nu2)
             m = Minuit(likelihood_fixedMu, nu1=0.0, nu2=0.0)
             m.migrad()
-            qPoints.append(m.fval)
-        return qPoints, muList
+            qDeltas.append(m.fval-q_mle)
+        return qDeltas, muList
 
     def impacts(self):
         print("Calculate impacts")
-
         # First find global Min and store MLE values
-        errordef = Minuit.LEAST_SQUARES
-        m = Minuit(self.likelihood_function, mu=0.0, nu1=0.0, nu2=0.0)
-        m.migrad()
-        q_mle = m.fval
-        mu_mle = m.values["mu"]
-        nu1_mle = m.values["nu1"]
-        nu2_mle = m.values["nu2"]
+        q_mle, parameters_mle = self.fit()
+        mu_mle = parameters_mle["mu"]
+        nu1_mle = parameters_mle["nu1"]
+        nu2_mle = parameters_mle["nu2"]
 
-        # Scan over nu1
-        upper_nu1, lower_nu1 = None, None
+        # Define functions that fix all parameters but a single nu
         nu1_function = lambda nu1: abs(self.likelihood_function(mu_mle, nu1, nu2_mle)-q_mle-1.0)
-        m_up = Minuit(nu1_function, nu1=nu1_mle+0.01)
-        m_up.limits["nu1"] = (nu1_mle, None)
-        m_up.migrad()
-        upper_nu1 = m_up.values["nu1"]
-
-        m_down = Minuit(nu1_function, nu1=nu1_mle-0.01)
-        m_down.migrad()
-        m_down.limits["nu1"] = (None, nu1_mle)
-        lower_nu1 = m_down.values["nu1"]
-
-        # Scan over nu2
-        upper_nu2, lower_nu2 = None, None
         nu2_function = lambda nu2: abs(self.likelihood_function(mu_mle, nu1_mle, nu2)-q_mle-1.0)
-        m_up = Minuit(nu2_function, nu2=nu2_mle+0.01)
-        m_up.limits["nu2"] = (nu2_mle, None)
-        m_up.migrad()
-        upper_nu2 = m_up.values["nu2"]
+        nu_functions = {
+            "nu1": nu1_function,
+            "nu2": nu2_function,
+        }
 
-        m_down = Minuit(nu2_function, nu2=nu2_mle-0.01)
-        m_down.migrad()
-        m_down.limits["nu2"] = (None, nu2_mle)
-        lower_nu2 = m_down.values["nu2"]
+        # Now go through each nu and find point where q - q_mle == 1
+        # Do this from nu_mle to both sides to find lower and upper boundaries
+        limits = {}
+        for nuname in ["nu1", "nu2"]:
+            upper, lower = None, None
 
-        return (upper_nu1, lower_nu1), (upper_nu2, lower_nu2)
+            # Use **kwargs in order to dynamically change the parameters (nu1 -> nu2, ....)
+            param_up = {nuname: parameters_mle[nuname] + 0.01}
+            m_up = Minuit(nu_functions[nuname], **param_up)
+            m_up.limits[nuname] = (parameters_mle[nuname], None)
+            m_up.migrad()
+            upper = m_up.values[nuname]
 
+            param_down = {nuname: parameters_mle[nuname] - 0.01}
+            m_down = Minuit(nu_functions[nuname], **param_down)
+            m_down.migrad()
+            m_down.limits[nuname] = (None, parameters_mle[nuname])
+            lower = m_down.values[nuname]
+            limits[nuname] = (lower, upper)
+        return limits
 
 if __name__ == "__main__":
+    print("1. Simple fit")
     fit = likelihoodFit(None)
     q_mle, parameters_mle = fit.fit()
     nu1_mle = parameters_mle["nu1"]
     nu2_mle = parameters_mle["nu2"]
-
-    print(q_mle, parameters_mle)
-    print("----------------")
-    qPoints,muPoints = fit.scan()
-    print(qPoints)
-    print(muPoints)
-    print("----------------")
-    (upper_nu1, lower_nu1), (upper_nu2, lower_nu2) = fit.impacts()
-    print(f"nu1 = {nu1_mle} + {upper_nu1-nu1_mle} - {nu1_mle-lower_nu1}")
-    print(f"nu2 = {nu2_mle} + {upper_nu2-nu2_mle} - {nu2_mle-lower_nu2}")
+    print(f" - q at MLE: {q_mle}")
+    print(f" - Parameters at MLE: {parameters_mle}")
+    print("-------------------------------------")
+    print("2. Scan over mu")
+    deltaQ,muPoints = fit.scan()
+    print(f" - mus = {muPoints}")
+    print(f" - deltaQs = {deltaQ}")
+    print("-------------------------------------")
+    print("3. Impacts")
+    limits = fit.impacts()
+    print(f" - constrained parameter boundaries: {limits}")
