1b536085057dd76edfb3fe87d1dc1708e4c6d1e8
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2024-12-26 17:11:15 +0100

    process support in ICP

--- ML/ICP/icp_training.sh
+++ ML/ICP/icp_training.sh
@@ -2,22 +2,22 @@
 #python icp_training.py --config icp_quad_tes_jes_met --selection highMT_VBFJet
 #python icp_training.py --config icp_quad_tes_jes_met --selection lowMT_noVBFJet_ptH0to100
 #python icp_training.py --config icp_quad_tes_jes_met --selection lowMT_noVBFJet_ptH100
-#python icp_training.py --config icp_quad_tes_jes_met --selection highMT_noVBFJet
+python icp_training.py --config icp_quad_tes_jes_met_fixed --selection highMT_noVBFJet
+#
+#python icp_training.py --config icp_quad_jes --selection lowMT_VBFJet
+#python icp_training.py --config icp_quad_jes --selection highMT_VBFJet
+#python icp_training.py --config icp_quad_jes --selection lowMT_noVBFJet_ptH0to100
+#python icp_training.py --config icp_quad_jes --selection lowMT_noVBFJet_ptH100
+#python icp_training.py --config icp_quad_jes --selection highMT_noVBFJet
+#
+#python icp_training.py --config icp_quad_tes --selection lowMT_VBFJet
+#python icp_training.py --config icp_quad_tes --selection highMT_VBFJet
+#python icp_training.py --config icp_quad_tes --selection lowMT_noVBFJet_ptH0to100
+#python icp_training.py --config icp_quad_tes --selection lowMT_noVBFJet_ptH100
+#python icp_training.py --config icp_quad_tes --selection highMT_noVBFJet
 
-python icp_training.py --config icp_quad_jes --selection lowMT_VBFJet
-python icp_training.py --config icp_quad_jes --selection highMT_VBFJet
-python icp_training.py --config icp_quad_jes --selection lowMT_noVBFJet_ptH0to100
-python icp_training.py --config icp_quad_jes --selection lowMT_noVBFJet_ptH100
-python icp_training.py --config icp_quad_jes --selection highMT_noVBFJet
-
-python icp_training.py --config icp_quad_tes --selection lowMT_VBFJet
-python icp_training.py --config icp_quad_tes --selection highMT_VBFJet
-python icp_training.py --config icp_quad_tes --selection lowMT_noVBFJet_ptH0to100
-python icp_training.py --config icp_quad_tes --selection lowMT_noVBFJet_ptH100
-python icp_training.py --config icp_quad_tes --selection highMT_noVBFJet
-
-#python icp_training.py --config icp_quad_met --selection lowMT_VBFJet
-#python icp_training.py --config icp_quad_met --selection highMT_VBFJet
-#python icp_training.py --config icp_quad_met --selection lowMT_noVBFJet_ptH0to100
-#python icp_training.py --config icp_quad_met --selection lowMT_noVBFJet_ptH100
-#python icp_training.py --config icp_quad_met --selection highMT_noVBFJet
+python icp_training.py --config icp_quad_met --selection lowMT_VBFJet
+python icp_training.py --config icp_quad_met --selection highMT_VBFJet
+python icp_training.py --config icp_quad_met --selection lowMT_noVBFJet_ptH0to100
+python icp_training.py --config icp_quad_met --selection lowMT_noVBFJet_ptH100
+python icp_training.py --config icp_quad_met --selection highMT_noVBFJet
