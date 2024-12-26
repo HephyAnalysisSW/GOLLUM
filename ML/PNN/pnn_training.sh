9fb200cadcefd6af51b931af439532f5cb76de40
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2024-12-26 17:13:28 +0100

    subdir support in PNN

--- ML/PNN/pnn_training.sh
+++ ML/PNN/pnn_training.sh
@@ -1,11 +1,43 @@
-python pnn_training.py --selection lowMT_VBFJet --config pnn_quad_jes_v2 --training v4 --every 1
-python pnn_training.py --selection lowMT_noVBFJet_ptH0to100 --config pnn_quad_jes_v2 --n_split 50 --training v4 --every 1
-python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_quad_jes_v2 --training v4 --every 1
-python pnn_training.py --selection highMT_VBFJet --config pnn_quad_jes_v2 --training v4 --every 1
-python pnn_training.py --selection highMT_noVBFJet --config pnn_quad_jes_v2 --training v4 --every 1
+#python pnn_training.py --selection lowMT_VBFJet --config pnn_quad_jes_v2 --training v4 --every 1
+#python pnn_training.py --selection lowMT_noVBFJet_ptH0to100 --config pnn_quad_jes_v2 --n_split 50 --training v4 --every 1
+#python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_quad_jes_v2 --training v4 --every 1
+#python pnn_training.py --selection highMT_VBFJet --config pnn_quad_jes_v2 --training v4 --every 1
+#python pnn_training.py --selection highMT_noVBFJet --config pnn_quad_jes_v2 --training v4 --every 1
+#
+#python pnn_training.py --selection lowMT_VBFJet --config pnn_quad_tes_v2 --training v5 --every 1
+#python pnn_training.py --selection lowMT_noVBFJet_ptH0to100 --config pnn_quad_tes_v2 --n_split 50 --training v5 --every 1
+#python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_quad_tes_v2 --training v5 --every 1
+#python pnn_training.py --selection highMT_VBFJet --config pnn_quad_tes_v2 --training v5 --every 1
+#python pnn_training.py --selection highMT_noVBFJet --config pnn_quad_tes_v2 --training v5 --every 1
+#
+#python pnn_training.py --selection lowMT_VBFJet --config pnn_quad_met_all --training v5 --every 1
+#python pnn_training.py --selection lowMT_noVBFJet_ptH0to100 --config pnn_quad_met_all --n_split 50 --training v5 --every 1
+##python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_quad_met_all --training v5 --every 1
+#python pnn_training.py --selection highMT_VBFJet --config pnn_quad_met_all --training v5 --every 1
+#python pnn_training.py --selection highMT_noVBFJet --config pnn_quad_met_all --training v5 --every 1
+#
+#python pnn_training.py --selection lowMT_VBFJet --config pnn_lin_jes --training v2 --every 1
+#python pnn_training.py --selection lowMT_noVBFJet_ptH0to100 --config pnn_lin_jes --n_split 50 --training v2 --every 1
+#python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_lin_jes --training v2 --every 1
+#python pnn_training.py --selection highMT_VBFJet --config pnn_lin_jes --training v2 --every 1
+#python pnn_training.py --selection highMT_noVBFJet --config pnn_lin_jes --training v2 --every 1
+#
+#python pnn_training.py --selection lowMT_VBFJet --config pnn_lin_tes --training v2 --every 1
+#python pnn_training.py --selection lowMT_noVBFJet_ptH0to100 --config pnn_lin_tes --n_split 50 --training v2 --every 1
+#python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_lin_tes --training v2 --every 1
+#python pnn_training.py --selection highMT_VBFJet --config pnn_lin_tes --training v2 --every 1
+#python pnn_training.py --selection highMT_noVBFJet --config pnn_lin_tes --training v2 --every 1
+#
+#python pnn_training.py --selection lowMT_VBFJet --config pnn_quad_met_all --training v5 --every 1
+#python pnn_training.py --selection lowMT_noVBFJet_ptH0to100 --config pnn_quad_met_all --n_split 50 --training v5 --every 1
+##python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_quad_met_all --training v5 --every 1
+#python pnn_training.py --selection highMT_VBFJet --config pnn_quad_met_all --training v5 --every 1
+#python pnn_training.py --selection highMT_noVBFJet --config pnn_quad_met_all --training v5 --every 1
+#
+# massive:
+python pnn_training.py --selection lowMT_VBFJet --config pnn_quad_tes_jes_met --training v1 --every 1
+python pnn_training.py --selection lowMT_noVBFJet_ptH0to100 --config pnn_quad_tes_jes_met --n_split 100 --training v1 --every 1
+python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_quad_tes_jes_met --training v1 --every 1
+#python pnn_training.py --selection highMT_VBFJet --config pnn_quad_tes_jes_met --training v1 --every 1
+python pnn_training.py --selection highMT_noVBFJet --config pnn_quad_tes_jes_met_fixed --training v1 --every 1
 
-python pnn_training.py --selection lowMT_VBFJet --config pnn_quad_tes_v2 --training v5 --every 1
-python pnn_training.py --selection lowMT_noVBFJet_ptH0to100 --config pnn_quad_tes_v2 --n_split 50 --training v5 --every 1
-python pnn_training.py --selection lowMT_noVBFJet_ptH100 --config pnn_quad_tes_v2 --training v5 --every 1
-python pnn_training.py --selection highMT_VBFJet --config pnn_quad_tes_v2 --training v5 --every 1
-python pnn_training.py --selection highMT_noVBFJet --config pnn_quad_tes_v2 --training v5 --every 1
