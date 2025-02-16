c709f775553180868390c1cc18acf9e8b1feb5f4
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2025-02-16 21:00:07 +0100

    Don't exp and multiply. Sum the logs and then exponentiate.

--- Workflow/plotLL2.sh
+++ Workflow/plotLL2.sh
@@ -1,13 +1,13 @@
-python plotLL2.py --postfix v3 --config config_reference.yaml --logLevel DEBUG --asimov_mu=2.5 --var mu         --range 2.7 2.9 0.01
-python plotLL2.py --postfix v3 --config config_reference.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_bkg     --range -0.5 0.5 0.01
-python plotLL2.py --postfix v3 --config config_reference.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_tt      --range -0.5 0.5 0.01
-python plotLL2.py --postfix v3 --config config_reference.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_diboson --range -0.5 0.5 0.01
-python plotLL2.py --postfix v3 --config config_reference.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_tes     --range -0.5 0.5 0.01
-python plotLL2.py --postfix v3 --config config_reference.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_jes     --range -0.5 0.5 0.01
-python plotLL2.py --postfix v3 --config config_reference.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_met     --range 0 0.5 0.01
+python plotLL2.py --postfix v4 --config configs/config_reference_v2.yaml --logLevel DEBUG --asimov_mu=2.5 --var mu         --range 2.78 2.82 0.002
+python plotLL2.py --postfix v4 --config configs/config_reference_v2.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_bkg     --range -0.1 0.1 0.002
+python plotLL2.py --postfix v4 --config configs/config_reference_v2.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_tt      --range -0.1 0.1 0.002
+python plotLL2.py --postfix v4 --config configs/config_reference_v2.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_diboson --range -0.1 0.1 0.002
+python plotLL2.py --postfix v4 --config configs/config_reference_v2.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_tes     --range -0.1 0.1 0.002
+python plotLL2.py --postfix v4 --config configs/config_reference_v2.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_jes     --range -0.1 0.1 0.002
+python plotLL2.py --postfix v4 --config configs/config_reference_v2.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_met     --range 0 0.1 0.002
 
 
-python plotLL2.py --postfix v3 --config config_reference.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_tt      --range -0.08 0.08 0.005
+#python plotLL2.py --postfix v3 --config configs/config_reference_v2.yaml --logLevel DEBUG --asimov_mu=2.5 --var nu_tt      --range -0.08 0.08 0.005
 
 
 
