1ac3ee674c0201fd895d89913cde4def5cf977ec
Claudius Krause <Claudius.Krause.21@gmail.com>
2025-02-27 15:03:41 +0100

    Split .sh into two files

---
+++ ML/Calibration/multiclasscalibration.sh
@@ -0,0 +1,3 @@
+python MulticlassCalibration.py --config config_reference_v2_calib --selection lowMT_VBFJet
+python MulticlassCalibration.py --config config_reference_v2_calib --selection lowMT_noVBFJet_ptH100
+python MulticlassCalibration.py --config config_reference_v2_calib --selection highMT_VBFJet
