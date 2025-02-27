84367d35f9c991a4a1cb531c239a05b0882a7891
Claudius Krause <Claudius.Krause.21@gmail.com>
2025-02-27 09:58:22 +0100

    Add predict() to calibration class; add multiclasscalibration class; add plotting of trained isotonic regression for validation for both classes

--- ML/Calibration/Calibration.py
+++ ML/Calibration/Calibration.py
@@ -10,6 +10,7 @@ sys.path.insert( 0, '../..')
 from common.logger import get_logger
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 import common.user as user
 import common.selections as selections
@@ -98,6 +99,171 @@ class Calibration:
 
         logger.info(f"Loaded Calibration {filename}")
 
+
+    def predict(self, input_dcr):
+        # assume for now that calibrator was trained / loaded
+        # TO-DO: check if calibrator exists and train/load otherwise
+        
+        output_dcr = input_dcr.copy() # to be overwritten below
+        calibrated_0_dcr = self.iso_reg.predict(input_dcr[:, 0]) # changes DCR value of class 0 only
+        output_dcr[:, 1:] = output_dcr[:, 1:] * ((1.-calibrated_0_dcr)/(1.-output_dcr[:, 0])).reshape(-1,1) # rescale DCR of remaining classes, such that sum stays 1
+        output_dcr[:, 0] = calibrated_0_dcr # put correct value in first column, too
+        return output_dcr
+
+    def plot_calibration(self, file_name):
+        pass
+
+    def plot_IsotonicRegression(self, file_name):
+        # assume for now that calibrator was trained / loaded
+        # TO-DO: check if calibrator exists and train/load otherwise
+        x_scan = np.linspace(0., 1., 1001)
+        plt.plot(x_scan, self.iso_reg.predict(x_scan), label="Isotonic Regression Calibrator")
+        plt.plot([0.,1.], [0.,1.], ls='dashed',c='k', label='identity')
+        plt.xlim(0., 1.)
+        plt.ylim(0., 1.)
+        plt.legend(loc="upper left")
+        plt.gcf().add_axes([0.575, 0.175, 0.3, 0.3])
+        x_scan = np.linspace(0., .2, 201)
+        plt.plot(x_scan, self.iso_reg.predict(x_scan), label="Isotonic Regression Calibrator")
+        plt.plot([0.,.2], [0.,.2], ls='dashed',c='k', label='identity')
+        plt.xlim(0., .2)
+        plt.ylim(0., .2)
+        plt.savefig(file_name)
+
+
+class MultiClassCalibration:
+
+    def __init__( self, yaml_config=None, selection=None, n_split=10, small=False):
+
+        self.selection   = selection
+        self.n_split     = n_split if not small else 100
+        self.small = small
+
+        if yaml_config is not None:
+            with open(yaml_config) as f:
+                cfg_ = yaml.safe_load(f)
+                self.cfg = cfg_['MultiClassifier'][selection]
+
+            logger.info("Config loaded from Workflow/configs/{}".format(args.config))
+
+            if self.cfg['module']=='TFMC':
+                from ML.TFMC.TFMC import TFMC
+                self.classifier = TFMC.load(self.cfg['model_path'])
+            elif self.cfg['module']=='XGBMC':
+                from ML.XGBMC.XGBMC import XGBMC
+                self.classifier = XGBMC.load(self.cfg['model_path'])
+            else:
+                raise NotImplementedError
+
+    def train( self ):
+        logger.info(f"Training: Load data for {self.selection}")
+
+        self.loader = datasets.get_data_loader(selection=self.selection, n_split=self.n_split)
+
+        # Initialize lists for accumulation
+        all_prob = []
+        all_weights = []
+        all_labels = []
+
+        for batch in tqdm(self.loader, desc="Processing batches"):
+            data, weights, labels = self.loader.split(batch)  # Unpack batch
+
+            prob = self.classifier.predict(data, ic_scaling=True)  # Predict with ic_scaling
+            all_prob.append(prob)
+            all_weights.append(weights)
+            all_labels.append(labels)
+
+            del data  # Explicitly delete data to free memory
+            if self.small: break
+
+        # Concatenate into final NumPy arrays
+        all_prob = np.concatenate(all_prob, axis=0)
+        all_weights = np.concatenate(all_weights, axis=0)
+        all_labels = np.concatenate(all_labels, axis=0)
+
+        all_prob/=all_prob.sum(axis=1, keepdims=True)
+
+        self.iso_reg = []
+        for class_id in range(4):
+            truth       = (all_labels == class_id).astype(float) # get truth label
+            logger.info( f"Training IsotonicRegression for class {class_id}." ) 
+            self.iso_reg.append(IsotonicRegression(out_of_bounds='clip', y_min=1e-6, y_max=1.-1e-6).fit(all_prob[:, class_id], truth, sample_weight=all_weights))
+            logger.info( f"Isotonic Regressions of class {class_id} done." ) 
+        logger.info( "All Isotonic Regressions Done." ) 
+        
+    def save( self, file_name ):
+        with open(filename, 'wb') as file:
+            pickle.dump(self.iso_reg, file)
+
+        logger.info(f"Written {filename}")
+
+    def load( self, file_name ):
+        with open(filename, 'rb') as file:
+            self.iso_reg = pickle.load(file)
+        logger.info(f"Loaded Calibration {filename}")
+
+
+    def predict(self, input_dcr):
+        # assume for now that calibrator was trained / loaded
+        # TO-DO: check if calibrator exists and train/load otherwise
+
+        calibrated_dcr = np.zeros_like(input_dcr)
+        for class_id in range(4):
+            calibrated_dcr[:, class_id] = self.iso_reg[class_id].predict(input_dcr[:,class_id])
+            # rescale each class independly based on the independenly trained calibrator
+
+        # keep 0 (signal) at its value and rescale the other 3 such that the 4 sum to 1. 
+        calibrated_dcr[:, 1:] = calibrated_dcr[:, 1:] * ((1.-calibrated_dcr[:, 0])/(calibrated_dcr[:, 1:].sum(axis=-1))).reshape(-1,1)
+        
+        return calibrated_dcr
+
+    def plot_calibration(self, file_name):
+        pass
+
+    def plot_IsotonicRegression(self, file_name):
+        fig, axs = plt.subplots(2, 2)
+        for idx, loc in enumerate([(0,0), (0,1), (1,0), (1,1)]):
+            x_scan = np.linspace(0., 1., 1001)
+            axs[loc].plot(x_scan, self.iso_reg[idx].predict(x_scan))
+            axs[loc].plot([0.,1.], [0.,1.], ls='dashed',c='k', label='identity')
+            axs[loc].set_title(f"class {idx} calibration")
+    
+        for ax in axs.flat:
+            ax.set(xlabel='dcr_input', ylabel='dcr_output', xlim=(0,1), ylim=(0,1))
+            ax.label_outer()
+    
+        for idx, loc in enumerate([(0,0), (0,1), (1,0), (1,1)]):
+            axx = axs[loc].inset_axes([0.625, 0.175, 0.3, 0.3])
+            x_scan = np.linspace(0., .2, 201)
+            axx.plot(x_scan, self.iso_reg[idx].predict(x_scan))
+            axx.plot([0.,.2], [0.,.2], ls='dashed',c='k', label='identity')
+            axx.set(xlim=(0,0.2), ylim=(0,0.2))
+        plt.savefig(file_name)
+
+
+def weighted_calibration(true_label, pred_output, nbins=10, weights=None):
+    # reproduces to a large extend 
+    # from sklearn.calibration import calibration_curve
+    # but adds the functionality to have weighted events
+    bins = np.linspace(pred_output.min(), pred_output.max(), nbins+1)
+    if weights is None:
+        weights = np.ones_like(true_label)
+    mask = (pred_output.reshape(-1,1) >= bins[:-1]) & (bins[1:] >= pred_output.reshape(-1,1))
+    true_fraction = []
+    pred_fraction = []
+    for bin_nr in range(nbins):
+        if len(weights[mask[:,bin_nr]]) == 0:
+            true_fraction.append(0.)
+        else:
+            true_fraction.append((weights[mask[:,bin_nr]]*true_label[mask[:,bin_nr]]).sum() /(weights[mask[:,bin_nr]]).sum()+1e-16)
+        if len(pred_output[mask[:,bin_nr]]) == 0:
+            pred_fraction.append(0.)
+        else:
+            pred_fraction.append((pred_output[mask[:,bin_nr]]).mean())
+    prob_true = np.array(true_fraction)
+    prob_pred = np.array(pred_fraction)
+    return prob_true, prob_pred        
+
 if __name__=="__main__":
     import argparse
     # Argument parser setup
@@ -109,6 +275,7 @@ if __name__=="__main__":
     parser.add_argument("--small", action="store_true", help="Run a subset.")
     parser.add_argument("--overwrite", action="store_true", help="Overwrite existing files.")
     parser.add_argument("--postfix", default = None, type=str,  help="Append this to the fit result.")
+    parser.add_argument("--multiclass", action="store_true", default=False, help="If set, do multiclass calibration instead of 0 vs rest.")
 
     args = parser.parse_args()
 
@@ -119,7 +286,7 @@ if __name__=="__main__":
     # Where to store the training
     model_directory = os.path.join(user.model_directory, "Calibration", *subdirs,  args.config, args.selection)
     os.makedirs(model_directory, exist_ok=True)
-    filename = os.path.join( model_directory, 'calibrator.pkl')
+    filename = os.path.join( model_directory, f'calibrator{"_multi" if args.multiclass else ""}.pkl')
 
     if os.path.exists( filename ) and not args.overwrite:
         logger.info(f"Found {filename}. Do nothing")
@@ -127,9 +294,17 @@ if __name__=="__main__":
     elif os.path.exists( filename ) and args.overwrite:
         logger.warning(f"Will overwrite {filename}")    
 
-    calib = Calibration( os.path.join( os.path.abspath('../../Workflow/configs'), args.config) +".yaml", selection=args.selection, small=args.small)
+    if args.multiclass:
+        calib = MultiClassCalibration( os.path.join( os.path.abspath('../../Workflow/configs'), args.config) +".yaml", selection=args.selection, small=args.small)
+
+    else:
+        calib = Calibration( os.path.join( os.path.abspath('../../Workflow/configs'), args.config) +".yaml", selection=args.selection, small=args.small)
 
     calib.train()
     
     calib.save(model_directory)
 
+    calib.plot_calibration(os.path.join( model_directory, 
+                                         f'calibrator_validation_calibration{"_multi" if args.multiclass else ""}.png'))
+    calib.plot_IsotonicRegression(os.path.join( model_directory, 
+                                                f'calibrator_validation_IsoReg{"_multi" if args.multiclass else ""}.png'))
