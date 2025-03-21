5aed4f5a7e54dd0d687642f9e47dc94f4b79b35f
Ang Li <Ang.Li@oeaw.ac.at>
2025-03-21 14:55:25 +0100

    clean up

--- common/user.py
+++ common/user.py
@@ -1,11 +1,16 @@
 import os
 
+data_directory         = "./train/"
+derived_data_directory = "./data/"
+derived_test_data_directory = "./test_data/"
+
 try:
     user = os.environ['USER']
 except:
     user = "noUserFound"
 
 tmp_mem_directory      = "/dev/shm/%s/"%(user)
+
 
 plot_directory  = "./plots/"
 model_directory = "./models/"
