7fd1fa2962a2a8eeaaff7d28d57b6851540a91db
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2025-10-19 16:20:16 +0200

    tao

---
+++ TAO/soft_training.sh
@@ -0,0 +1,6 @@
+python soft_training.py --logLevel DEBUG --data toy --forest_config configs/tree_soft_toy_v1.yaml --train_config configs/training_softtree.yaml --overwrite --every 20
+python soft_training.py --logLevel DEBUG --data toy --forest_config configs/tree_soft_toy_v1.yaml --train_config configs/training_softtree_t0p1.yaml --overwrite --every 20
+python soft_training.py --logLevel DEBUG --data toy --forest_config configs/tree_soft_toy_v1.yaml --train_config configs/training_softtree_t0p5.yaml --overwrite --every 20
+python soft_training.py --logLevel DEBUG --data toy --forest_config configs/tree_soft_toy_v1.yaml --train_config configs/training_softtree_t2p0.yaml --overwrite --every 20
+python soft_training.py --logLevel DEBUG --data toy --forest_config configs/tree_soft_toy_v1.yaml --train_config configs/training_softtree_t3p0.yaml --overwrite --every 20
+
