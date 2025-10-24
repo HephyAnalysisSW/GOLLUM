142f1b3408a37ec1346f9afaf7e2a1e88b8a01e2
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2025-10-24 07:43:38 +0200

    cleanup

--- ML/Scaler/demo.py
+++ ML/Scaler/demo.py
@@ -3,7 +3,7 @@ sys.path.insert(0, '..')
 sys.path.insert(0, '../..')
 
 from ML.Scaler.Scaler import Scaler 
-scaler = Scaler.load("/groups/hephy/cms/robert.schoefbeck/Challenge/models/Scaler/Scaler_lowMT_VBFJet.pkl")
+scaler = Scaler.load("/groups/hephy/cms/robert.schoefbeck/SBIPDF/models/Scaler/Scaler_tt2l_inclusive.pkl")
 
 print(scaler.feature_means)
 print(scaler.feature_variances)
