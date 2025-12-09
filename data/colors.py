e080c342c6304b80ab10fd7b9724e73aebfc74fc
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2025-12-09 18:54:06 +0100

    add missing colors

---
+++ data/colors.py
@@ -0,0 +1,26 @@
+import cmsstyle
+# https://cms-analysis.docs.cern.ch/guidelines/plotting/colors/#categorical-data-eg-1d-stackplots
+# https://cms-analysis.docs.cern.ch/guidelines/plotting/examples/#stack-plot-with-cmsstyle
+#cmsstyle.p10.kBlue
+#cmsstyle.p10.kYellow
+#cmsstyle.p10.kRed
+#cmsstyle.p10.kAsh
+#cmsstyle.p10.kViolet
+#cmsstyle.p10.kBrown
+#cmsstyle.p10.kOrange
+#cmsstyle.p10.kGreen
+#cmsstyle.p10.kGray
+#cmsstyle.p10.kCyan
+
+colors = {
+ 'TTLep_pow':   cmsstyle.p10.kRed,
+ 'DrellYan':    cmsstyle.p10.kGreen,
+ 'SingleTop':   cmsstyle.p10.kYellow,
+ 'TTSemi_pow':  cmsstyle.p10.kOrange,
+}
+
+def get_color( sample_name ):
+    for k, c in colors.items():
+        if sample_name.startswith( k ):
+            return c
+
