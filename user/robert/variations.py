0899b8ab6978c367a2f0a347ce0bd6f0f580a9cf
Ricardo Barrue <ricardo.barrue@cern.ch>
2026-03-04 11:33:24 +0100

    Updating variations in plotting script

--- user/robert/variations.py
+++ user/robert/variations.py
@@ -36,33 +36,6 @@ variations = OrderedDict( {
     'res4_2018'    : {'sys_up': 'CMS_res_j_4_2018_up', 'sys_down':'CMS_res_j_4_2018_down', 'eras':['2018']},
     'res5_2018'    : {'sys_up': 'CMS_res_j_5_2018_up', 'sys_down':'CMS_res_j_5_2018_down', 'eras':['2018']},
 
-    'jes_b'        : {'sys_up': 'CMS_scale_j_FlavorPureBottom_up',      'sys_down': 'CMS_scale_j_FlavorPureBottom_down'},
-    'jes_c'        : {'sys_up': 'CMS_scale_j_FlavorPureCharm_up',       'sys_down': 'CMS_scale_j_FlavorPureCharm_down'},
-    'jeq_g'        : {'sys_up': 'CMS_scale_j_FlavorPureGluon_up',       'sys_down': 'CMS_scale_j_FlavorPureGluon_down'},
-    'jes_q'        : {'sys_up': 'CMS_scale_j_FlavorPureQuark_up',       'sys_down': 'CMS_scale_j_FlavorPureQuark_down'},
-    'jes_abs'      : {'sys_up': 'CMS_scale_j_Regrouped_Absolute_up',    'sys_down': 'CMS_scale_j_Regrouped_Absolute_down'},
-    'jes_BBEC1'    : {'sys_up': 'CMS_scale_j_Regrouped_BBEC1_up',       'sys_down': 'CMS_scale_j_Regrouped_BBEC1_down'},
-    'jes_EC2'      : {'sys_up': 'CMS_scale_j_Regrouped_EC2_up',         'sys_down': 'CMS_scale_j_Regrouped_EC2_down'},
-    'jes_HF'       : {'sys_up': 'CMS_scale_j_Regrouped_HF_up',          'sys_down': 'CMS_scale_j_Regrouped_HF_down'},
-    'jes_relbal'   : {'sys_up': 'CMS_scale_j_Regrouped_RelativeBal_up', 'sys_down': 'CMS_scale_j_Regrouped_RelativeBal_down'},
-
-    'jes_abs_16'   : {'sys_up': 'CMS_scale_j_Regrouped_Absolute_2016_up',       'sys_down': 'CMS_scale_j_Regrouped_Absolute_2016_down',       'eras':['2016APV', '2016']},
-    'jes_BBEC1_16' : {'sys_up': 'CMS_scale_j_Regrouped_BBEC1_2016_up',          'sys_down': 'CMS_scale_j_Regrouped_BBEC1_2016_down',          'eras':['2016APV', '2016']},
-    'jes_EC2_16'   : {'sys_up': 'CMS_scale_j_Regrouped_EC2_2016_up',            'sys_down': 'CMS_scale_j_Regrouped_EC2_2016_down',            'eras':['2016APV', '2016']},
-    'jes_HF_16'    : {'sys_up': 'CMS_scale_j_Regrouped_HF_2016_up',             'sys_down': 'CMS_scale_j_Regrouped_HF_2016_down',             'eras':['2016APV', '2016']},
-    'jes_relsam_16': {'sys_up': 'CMS_scale_j_Regrouped_RelativeSample_2016_up', 'sys_down': 'CMS_scale_j_Regrouped_RelativeSample_2016_down', 'eras':['2016APV', '2016']},
-
-    'jes_abs_17'   : {'sys_up': 'CMS_scale_j_Regrouped_Absolute_2017_up',        'sys_down':'CMS_scale_j_Regrouped_Absolute_2017_down',       'eras':['2017']},
-    'jes_BBEC1_17' : {'sys_up': 'CMS_scale_j_Regrouped_BBEC1_2017_up',          'sys_down': 'CMS_scale_j_Regrouped_BBEC1_2017_down',          'eras':['2017']},
-    'jes_EC2_17'   : {'sys_up': 'CMS_scale_j_Regrouped_EC2_2017_up',            'sys_down': 'CMS_scale_j_Regrouped_EC2_2017_down',            'eras':['2017']},
-    'jes_HF_17'    : {'sys_up': 'CMS_scale_j_Regrouped_HF_2017_up',             'sys_down': 'CMS_scale_j_Regrouped_HF_2017_down',             'eras':['2017']},
-    'jes_relsam_17': {'sys_up': 'CMS_scale_j_Regrouped_RelativeSample_2017_up', 'sys_down': 'CMS_scale_j_Regrouped_RelativeSample_2017_down', 'eras':['2017']},
-
-    'jes_abs_18'   : {'sys_up': 'CMS_scale_j_Regrouped_Absolute_2018_up',       'sys_down': 'CMS_scale_j_Regrouped_Absolute_2018_down',       'eras':['2018']},
-    'jes_BBEC1_18' : {'sys_up': 'CMS_scale_j_Regrouped_BBEC1_2018_up',          'sys_down': 'CMS_scale_j_Regrouped_BBEC1_2018_down',          'eras':['2018']},
-    'jes_EC2_18'   : {'sys_up': 'CMS_scale_j_Regrouped_EC2_2018_up',            'sys_down': 'CMS_scale_j_Regrouped_EC2_2018_down',            'eras':['2018']},
-    'jes_HF_18'    : {'sys_up': 'CMS_scale_j_Regrouped_HF_2018_up',             'sys_down': 'CMS_scale_j_Regrouped_HF_2018_down',             'eras':['2018']},
-    'jes_relsam_18': {'sys_up': 'CMS_scale_j_Regrouped_RelativeSample_2018_up', 'sys_down': 'CMS_scale_j_Regrouped_RelativeSample_2018_down', 'eras':['2018']},
 
     'jme_uncl'     : {'sys_up': 'Uncl_up', 'sys_down': 'Uncl_down'},
 
@@ -72,6 +45,14 @@ variations = OrderedDict( {
     'btag_l'       : {'removeweight':'btagSF_fixedWP_SF', 'weight_up': 'btagSF_fixedWP_SF__CMS_eff_b_light_SFUp', 'weight_down':'btagSF_fixedWP_SF__CMS_eff_b_light_SFDn'}, 
     'pu'           : {'removeweight':'Pileup_SF', 'weight_up': 'Pileup_SFUp', 'weight_down':'Pileup_SFDn'}, 
     'l1pre'        : {'removeweight':'L1PreFiringWeight_Nom', 'weight_up':'L1PreFiringWeight_Up', 'weight_down':'L1PreFiringWeight_Dn'},
+    
+    'jes_total_EtaBin0'        : {'sys_up': 'CMS_scale_j_Total_EtaBin0_up',      'sys_down': 'CMS_scale_j_Total_EtaBin0_down'},
+    'jes_total_EtaBin1'        : {'sys_up': 'CMS_scale_j_Total_EtaBin1_up',      'sys_down': 'CMS_scale_j_Total_EtaBin1_down'},
+    'jes_total_EtaBin2'        : {'sys_up': 'CMS_scale_j_Total_EtaBin2_up',      'sys_down': 'CMS_scale_j_Total_EtaBin2_down'},
+    'jes_total_EtaBin3'        : {'sys_up': 'CMS_scale_j_Total_EtaBin3_up',      'sys_down': 'CMS_scale_j_Total_EtaBin3_down'},
+    'jes_total_EtaBin4'        : {'sys_up': 'CMS_scale_j_Total_EtaBin4_up',      'sys_down': 'CMS_scale_j_Total_EtaBin4_down'},
+    'jes_total_EtaBin5'        : {'sys_up': 'CMS_scale_j_Total_EtaBin5_up',      'sys_down': 'CMS_scale_j_Total_EtaBin5_down'},
+    
     })
 
 syst_groups = {
@@ -85,14 +66,8 @@ syst_groups = {
         'res0_2017', 'res1_2017', 'res2_2017', 'res3_2017', 'res4_2017', 'res5_2017', 
         'res0_2018', 'res1_2018', 'res2_2018', 'res3_2018', 'res4_2018', 'res5_2018', 
     ],
-    'JES1': ['jes_b', 'jes_c', 'jeq_g', 'jes_q', 'jes_abs',  
-    ],
-    'JES2': [
-            'jes_BBEC1', 'jes_EC2', 'jes_HF', 'jes_relbal', 'jme_uncl',
-    ],
-    'JES3': [ 
-        'jes_abs_16', 'jes_BBEC1_16', 'jes_EC2_16', 'jes_HF_16', 'jes_relsam_16', 
-        'jes_abs_17', 'jes_BBEC1_17', 'jes_EC2_17', 'jes_HF_17', 'jes_relsam_17', 
-        'jes_abs_18', 'jes_BBEC1_18', 'jes_EC2_18', 'jes_HF_18', 'jes_relsam_18', 
+    'JESTOTAL' : [
+        'jes_total_EtaBin0', 'jes_total_EtaBin1', 'jes_total_EtaBin2',
+        'jes_total_EtaBin3', 'jes_total_EtaBin4', 'jes_total_EtaBin5'
     ]
     }
