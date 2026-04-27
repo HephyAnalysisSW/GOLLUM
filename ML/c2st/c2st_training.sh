41f1aa81ec6047a4c6b95f69c96ec3a00fa526e7
Ricardo Barrué <61161376+rbarrue@users.noreply.github.com>
2026-04-27 11:40:23 +0200

    Add PNN C2ST and BIT calibration plots (#119)
    Adds Classifier Two-Sample Test (C2ST) for high-dimensional systematics surrogate calibration checks
    Adds BIT output calibration plots for each of the components as well as a global calibration plot
    
    ---------
    
    Co-authored-by: Robert Schoefbeck <robert.schoefbeck@cern.ch>
    Co-authored-by: Daohan Wang <daohan.wang@oeaw.ac.at>
    Co-authored-by: Daohan Wang <wdh9508@gmail.com>

---
+++ ML/c2st/c2st_training.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+#SBATCH --job-name=traina
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=6
+#SBATCH --mem=20G
+#SBATCH --time=0-00:39:00
+#SBATCH --qos=short
+#SBATCH --output=traina.stdout
+
+echo "Job started at: $(date)"
+
+python -u dnn_training.py unbinned_2017.yaml --job c2st_test1_TTLep_pow_2017_CMS_res_j_0_2017 --n_split 5 --train_seed 43                                                                
+
+echo "Job finished at: $(date)"
