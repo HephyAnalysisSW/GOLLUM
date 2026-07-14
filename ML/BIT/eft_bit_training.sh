15a75e5d9de31a4993d70bf5a5debce708b42dd8
Ricardo Barrue <ricardo.barrue@cern.ch>
2026-07-14 22:40:24 +0200

    updating EFT BIT training configs + changing launch script

--- ML/BIT/eft_bit_training.sh
+++ ML/BIT/eft_bit_training.sh
@@ -1,17 +1,22 @@
-submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2016APV"
-submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2016"
-submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2017"
-submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2018"
-submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2016APV_allWC"
-submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2016_allWC"
-submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2017_allWC"
-submit --memory 50 --title bit --walltime 02-00:00:00 --queue medium  "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2018_allWC"
+submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft.yaml --every 0 --job bit_TT01j2l_EFT_2016_allWC" --output=eft_bit_training_15072026/ 
 
-submit --memory 35 --title bit8h "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2016APV"
-submit --memory 35 --title bit8h "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2016"
-submit --memory 35 --title bit8h "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2017"
-submit --memory 35 --title bit8h "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2018"
-submit --memory 35 --title bit8h "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2016APV_allWC"
-submit --memory 35 --title bit8h "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2016_allWC"
-submit --memory 35 --title bit8h "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2017_allWC"
-submit --memory 50 --title bit8h "python eft_bit_training.py ../../configs/eft/unbinned_gen_ND.yaml --every 0 --job bit_TT01j2l_EFT_2018_allWC"
+submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft.yaml --every 0 --job bit_TT01j2l_EFT_2016_ML4EFTWC" --output=eft_bit_training_15072026/
+
+submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft.yaml --every 0 --job bit_TT01j2l_EFT_2016_nonML4EFTWC" --output=eft_bit_training_15072026/
+
+# submit --memory 35 --title bit8h "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft.yaml --every 0 --job bit_TT01j2l_EFT_2016_ctG_only"
+
+# submit --memory 35 --title bit8h "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft.yaml --every 0 --job bit_TT01j2l_EFT_2016_ct_nonML4EFT"
+# submit --memory 35 --title bit8h "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft.yaml --every 0 --job bit_TT01j2l_EFT_2016_ct_ML4EFT"
+
+# submit --memory 35 --title bit8h "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft.yaml --every 0 --job bit_TT01j2l_EFT_2016_cQ_nonML4EFT_block01"
+# submit --memory 35 --title bit8h "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft.yaml --every 0 --job bit_TT01j2l_EFT_2016_cQ_nonML4EFT_block02"
+
+# submit --memory 35 --title bit8h "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft.yaml --every 0 --job bit_TT01j2l_EFT_2016_cQ_ML4EFT_block01"
+# submit --memory 35 --title bit8h "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft.yaml --every 0 --job bit_TT01j2l_EFT_2016_cQ_ML4EFT_block02"
+
+submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft_ND.yaml --every 0 --job bit_TT01j2l_EFT_2016_allWC" --output=eft_bit_training_15072026/
+
+submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft_ND.yaml --every 0 --job bit_TT01j2l_EFT_2016_ML4EFTWC" --output=eft_bit_training_15072026/
+
+submit --memory 35 --title bit --walltime 02-00:00:00 --queue medium  "python /users/ricardo.barrue/nsbi_gluon_pdf/GOLLUM/ML/BIT/eft_bit_training.py configs/unbinned_v7_eft/bit_training/unbinned_2016_eft_ND.yaml --every 0 --job bit_TT01j2l_EFT_2016_nonML4EFTWC" --output=eft_bit_training_15072026/
